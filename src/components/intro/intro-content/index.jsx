a33a02a3e916791a10f4be6cbb483e16d68db16b
babinicz <babinicz@onet.pl>
2024-12-16 16:55:41 +0100

    avif

--- src/components/intro/intro-content/index.jsx
+++ src/components/intro/intro-content/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.scss";
 import hand from "../../../images/hand.png";
 import CallToAction from "../../shared/callToAction";
-import img from "../../../images/8.png";
+import img from "../../../images/8.avif";
 import { BsAwardFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
 
